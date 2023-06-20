702c6eb111c9b499701cfd71593a3f015c76c96c
Denise Herrera <herreradenise930@gmail.com>
2023-06-20 15:33:39 -0300

    actualización de git hub

--- script.js
+++ script.js
@@ -2,30 +2,30 @@ function showAlert() {
     alert('¡Hola, esta es una alerta desde JavaScript!');
  }
 
- const form = document.getElementById("myForm")
+ const form = document.getElementById("myForm");
 
  function validateEmail(email) {
-    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
-    return regex.test(email)
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/;
+    return regex.test(email);
 }
 
 function validateForm() {
-    const input = document.getElementById("email")
-    const inputValue = input.value 
+    const input = document.getElementById("email");
+    const inputValue = input.value; 
 
     if (!validateEmail(inputValue)) {
-        alert('Por favor ingrese correo electrónico válido.')
+        alert('Por favor ingrese correo electrónico válido.');
     } else {
-        alert('Correo electrónico enviado correctamente.')
+        alert('Correo electrónico enviado correctamente.');
     }
 }
 
-form.addEventListener("submit", function(lala) {
-    lala.preventDefault()
+form.addEventListener("submit", function(event) {
+    event.preventDefault();
     validateForm();
-})
+});
 
-document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-           document.querySelector(".nav-links").
-                      classList.toggle("nav-links-responsive")})
+document.querySelector(".button-menu-toggle").addEventListener("click", function() {
+    document.querySelector(".nav-links").classList.toggle("nav-links-responsive");
+  });
+  
